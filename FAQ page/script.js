f80507f3a47ad45d38778ae3a7113ab66fde13be
SorisAnish17 <125728458+SorisAnish17@users.noreply.github.com>
2023-06-05 16:01:39 +0530

    Delete script.js

--- FAQ page/script.js
+++
@@ -1,8 +0,0 @@
-let btn = document.querySelectorAll("#btn");
-//btn function
-for (let button of btn) {
-  button.addEventListener("click", () => {
-    const parent = button.parentElement.parentElement;
-    parent.classList.toggle("answer");
-  });
-}
